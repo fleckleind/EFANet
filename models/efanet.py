269c9a58b9348d9b92d1072996fdbb807bfccaef
Haiyun Huang <81820136+fleckleind@users.noreply.github.com>
2026-04-25 10:26:47 +0800

    Update efanet.py

--- models/efanet.py
+++ models/efanet.py
@@ -1,1 +1,441 @@
+import math
+import pywt
+import torch
+from torch import nn
+import torch.nn.functional as F
+from timm.models.layers import trunc_normal_
 
+
+def create_2d_wavelet_filter(wave, in_size, out_size, type=torch.float):
+    w = pywt.Wavelet(wave)
+    dec_hi = torch.tensor(w.dec_hi[::-1], dtype=type)
+    dec_lo = torch.tensor(w.dec_lo[::-1], dtype=type)
+    dec_filters = torch.stack([
+        dec_lo.unsqueeze(0) * dec_lo.unsqueeze(1), dec_lo.unsqueeze(0) * dec_hi.unsqueeze(1),
+        dec_hi.unsqueeze(0) * dec_lo.unsqueeze(1), dec_hi.unsqueeze(0) * dec_hi.unsqueeze(1)], dim=0)
+    dec_filters = dec_filters[:, None].repeat(in_size, 1, 1, 1)
+
+    rec_hi = torch.tensor(w.rec_hi, dtype=type)
+    rec_lo = torch.tensor(w.rec_lo, dtype=type)
+    rec_filters = torch.stack([
+        rec_lo.unsqueeze(0) * rec_lo.unsqueeze(1), rec_lo.unsqueeze(0) * rec_hi.unsqueeze(1),
+        rec_hi.unsqueeze(0) * rec_lo.unsqueeze(1), rec_hi.unsqueeze(0) * rec_hi.unsqueeze(1)], dim=0)
+    rec_filters = rec_filters[:, None].repeat(out_size, 1, 1, 1)
+    return dec_filters, rec_filters
+
+
+def wavelet_2d_transform(x, filters):
+    b, c, h, w = x.shape
+    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    x = F.conv2d(x, filters, stride=2, groups=c, padding=pad)
+    x = x.reshape(b, c, 4, h // 2, w // 2)
+    return x
+
+
+def inverse_2d_wavelet_transform(x, filters):
+    b, c, _, h_half, w_half = x.shape
+    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    x = x.reshape(b, c * 4, h_half, w_half)
+    x = F.conv_transpose2d(x, filters, stride=2, groups=c, padding=pad)
+    return x
+
+
+class WTConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=5, stride=1, bias=True, wt_levels=1, wt_type='db1'):
+        super(WTConv2d, self).__init__()
+
+        self.in_channels = in_channels
+        self.wt_levels = wt_levels
+        self.stride = stride
+        self.dilation = 1
+
+        self.wt_filter, self.iwt_filter = create_2d_wavelet_filter(
+            wt_type, in_channels, in_channels, torch.float)
+        self.wt_filter = nn.Parameter(self.wt_filter, requires_grad=False)
+        self.iwt_filter = nn.Parameter(self.iwt_filter, requires_grad=False)
+
+        self.base_conv = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size, padding='same', stride=1, dilation=1, groups=in_channels, bias=bias),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, padding="same", stride=1, bias=bias),)
+        self.base_scale = _ScaleModule([1,out_channels,1,1])
+        self.resd_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=bias)
+
+        self.wavelet_convs = nn.ModuleList(
+            [nn.Conv2d(in_channels*4, in_channels*4, kernel_size, padding='same', stride=1,
+                       dilation=1, groups=in_channels*4, bias=False) for _ in range(self.wt_levels)])
+        self.wavelet_scale = nn.ModuleList(
+            [_ScaleModule([1,in_channels*4,1,1], init_scale=0.1) for _ in range(self.wt_levels)])
+        self.do_stride = nn.AvgPool2d(kernel_size=1, stride=stride) if self.stride > 1 else None
+
+    def forward(self, x):
+
+        x_ll_in_levels = []
+        x_h_in_levels = []
+        shapes_in_levels = []
+
+        curr_x_ll = x
+
+        for i in range(self.wt_levels):
+            curr_shape = curr_x_ll.shape
+            shapes_in_levels.append(curr_shape)
+            if (curr_shape[2] % 2 > 0) or (curr_shape[3] % 2 > 0):
+                curr_pads = (0, curr_shape[3] % 2, 0, curr_shape[2] % 2)
+                curr_x_ll = F.pad(curr_x_ll, curr_pads)
+
+            curr_x = wavelet_2d_transform(curr_x_ll, self.wt_filter)
+            curr_x_ll = curr_x[:,:,0,:,:]
+            
+            shape_x = curr_x.shape
+            curr_x_tag = curr_x.reshape(shape_x[0], shape_x[1] * 4, shape_x[3], shape_x[4])
+            curr_x_tag = self.wavelet_scale[i](self.wavelet_convs[i](curr_x_tag))
+            curr_x_tag = curr_x_tag.reshape(shape_x)
+
+            x_ll_in_levels.append(curr_x_tag[:,:,0,:,:])
+            x_h_in_levels.append(curr_x_tag[:,:,1:4,:,:])
+
+        next_x_ll = 0
+
+        for i in range(self.wt_levels-1, -1, -1):
+            curr_x_ll = x_ll_in_levels.pop()
+            curr_x_h = x_h_in_levels.pop()
+            curr_shape = shapes_in_levels.pop()
+
+            curr_x_ll = curr_x_ll + next_x_ll
+
+            curr_x = torch.cat([curr_x_ll.unsqueeze(2), curr_x_h], dim=2)
+            next_x_ll = inverse_2d_wavelet_transform(curr_x, self.iwt_filter)
+
+            next_x_ll = next_x_ll[:, :, :curr_shape[2], :curr_shape[3]]
+
+        x_tag = next_x_ll
+        assert len(x_ll_in_levels) == 0
+        
+        x = self.base_scale(self.base_conv(x))
+        x = x + self.resd_conv(x_tag)
+        
+        if self.do_stride is not None:
+            x = self.do_stride(x)
+
+        return x
+
+
+class _ScaleModule(nn.Module):
+    def __init__(self, dims, init_scale=1.0, init_bias=0):
+        super(_ScaleModule, self).__init__()
+        self.dims = dims
+        self.weight = nn.Parameter(torch.ones(*dims) * init_scale)
+        self.bias = None
+    
+    def forward(self, x):
+        return torch.mul(self.weight, x)
+
+
+class DepthWiseConv2d(nn.Module):
+    def __init__(self, dim_in, dim_out, kernel_size=3, padding=1, stride=1, dilation=1):
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(dim_in, dim_in, kernel_size=kernel_size, padding=padding, 
+                      stride=stride, dilation=dilation, groups=dim_in)
+        self.norm_layer = nn.GroupNorm(4, dim_in)
+        self.conv2 = nn.Conv2d(dim_in, dim_out, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv2(self.norm_layer(self.conv1(x)))
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+            
+
+class ChanAttn(nn.Module):
+    def __init__(self, dim, ratio=2):
+        super(ChanAttn, self).__init__()
+        self.avg_pool, self.max_pool = nn.AdaptiveAvgPool2d(1), nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(dim, dim // ratio, kernel_size=1, bias=False), nn.ReLU(inplace=True),
+            nn.Conv2d(dim // ratio, dim, kernel_size=1, bias=False), nn.Sigmoid(),)
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        return avg_out + max_out
+
+
+class SpatAttn(nn.Module):
+    def __init__(self,):
+        super(SpatAttn, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(2, 1, kernel_size=7, padding=3, bias=True), nn.Sigmoid(),)
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        nax_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, nax_out], dim=1)
+        return self.conv(x)
+    
+
+class MLFA(nn.Module):
+    def __init__(self, dim_xh, dim_xl, k_size=3, d_list=[1, 2, 5, 7]):
+        super().__init__()
+        group_size = dim_xl // 4
+        self.pre_project = nn.Conv2d(dim_xh, dim_xl, 1)
+        
+        self.spat_attn0, self.chan_attn0 = SpatAttn(), ChanAttn(group_size)
+        self.g0 = nn.Sequential(
+            LayerNorm(normalized_shape=group_size+1, data_format='channels_first'),
+            nn.Conv2d(group_size + 1, group_size + 1, kernel_size=3, stride=1, 
+                      padding=(k_size+(k_size-1)*(d_list[0]-1))//2, 
+                      dilation=d_list[0], groups=group_size+1))
+
+        self.spat_attn1, self.chan_attn1 = SpatAttn(), ChanAttn(group_size)
+        self.g1 = nn.Sequential(
+            LayerNorm(normalized_shape=group_size+1, data_format='channels_first'),
+            nn.Conv2d(group_size + 1, group_size + 1, kernel_size=3, stride=1, 
+                      padding=(k_size+(k_size-1)*(d_list[1]-1))//2, 
+                      dilation=d_list[1], groups=group_size+1))
+
+        self.spat_attn2, self.chan_attn2 = SpatAttn(), ChanAttn(group_size)
+        self.g2 = nn.Sequential(
+            LayerNorm(normalized_shape=group_size+1, data_format='channels_first'),
+            nn.Conv2d(group_size + 1, group_size + 1, kernel_size=3, stride=1, 
+                      padding=(k_size+(k_size-1)*(d_list[2]-1))//2, 
+                      dilation=d_list[2], groups=group_size+1))
+
+        self.spat_attn3, self.chan_attn3 = SpatAttn(), ChanAttn(group_size)
+        self.g3 = nn.Sequential(
+            LayerNorm(normalized_shape=group_size+1, data_format='channels_first'),
+            nn.Conv2d(group_size+1, group_size+1, kernel_size=3, stride=1, 
+                      padding=(k_size+(k_size-1)*(d_list[3]-1))//2, 
+                      dilation=d_list[3], groups=group_size+1))
+        self.tail_conv = nn.Sequential(
+            LayerNorm(normalized_shape=dim_xl + 4, data_format='channels_first'),
+            nn.Conv2d(dim_xl + 4, dim_xl, 1))
+        
+    def forward(self, xh, xl, mask):
+        xh = self.pre_project(xh)
+        xh = F.interpolate(xh, size=[xl.size(2), xl.size(3)], mode ='bilinear', align_corners=True)
+        xh, xl = torch.chunk(xh, 4, dim=1), torch.chunk(xl, 4, dim=1)
+        x0 = self.g0(torch.cat((xl[0]*self.spat_attn0(xl[0])*self.chan_attn0(xh[0]), mask), dim=1))
+        x1 = self.g1(torch.cat((xl[1]*self.spat_attn1(xl[1])*self.chan_attn1(xh[1]), mask), dim=1))
+        x2 = self.g2(torch.cat((xl[2]*self.spat_attn2(xl[2])*self.chan_attn2(xh[2]), mask), dim=1))
+        x3 = self.g3(torch.cat((xl[3]*self.spat_attn3(xl[3])*self.chan_attn3(xh[3]), mask), dim=1))
+        x = self.tail_conv(torch.cat((x0, x1, x2, x3), dim=1))
+        return x
+
+
+class MDFA(nn.Module):
+    def __init__(self, dim_in, dim_out, x=8, y=8):
+        super().__init__()
+
+        # convolution params
+        c_dim_in = dim_in // 4
+        self.c_dim_in = c_dim_in
+        k_size = 3
+        pad = (k_size-1) // 2
+        
+        self.params_xy = nn.Parameter(torch.Tensor(1, c_dim_in, x, y), requires_grad=True)
+        nn.init.ones_(self.params_xy)
+        self.conv_xy = nn.Sequential(
+            nn.Conv2d(c_dim_in, c_dim_in, kernel_size=k_size, padding=pad, groups=c_dim_in),
+            nn.GELU(), nn.Conv2d(c_dim_in, c_dim_in, 1))
+
+        self.attn_zx = nn.Sequential(
+            nn.AdaptiveAvgPool2d((None, 1)),
+            nn.Conv2d(c_dim_in, c_dim_in//4, 1), nn.GELU(),
+            nn.Conv2d(c_dim_in//4, c_dim_in, 1), nn.Sigmoid(),)
+
+        self.attn_zy = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, None)),
+            nn.Conv2d(c_dim_in, c_dim_in//4, 1), nn.GELU(),
+            nn.Conv2d(c_dim_in//4, c_dim_in, 1), nn.Sigmoid(),)
+
+        self.dw = nn.Sequential(
+                nn.Conv2d(c_dim_in, c_dim_in, 1), nn.GELU(),
+                nn.Conv2d(c_dim_in, c_dim_in, kernel_size=3, padding=1, groups=c_dim_in))
+        
+        self.norm1 = LayerNorm(dim_in, eps=1e-6, data_format='channels_first')
+        self.norm2 = LayerNorm(dim_in, eps=1e-6, data_format='channels_first')
+        
+        self.ldw = nn.Sequential(
+                nn.Conv2d(dim_in, dim_in, kernel_size=3, padding=1, groups=dim_in),
+                nn.GELU(), nn.Conv2d(dim_in, dim_out, 1),)
+        
+    def forward(self, x):
+        x = self.norm1(x)
+        x1, x2, x3, x4 = torch.chunk(x, 4, dim=1)
+        B, C, H, W = x1.size()
+        #----------xy----------#
+        params_xy = self.params_xy  # (1, c_dim_in, x, y)
+        x1 = x1 * self.conv_xy(F.interpolate(
+            params_xy, size=x1.shape[2:4],mode='bilinear', align_corners=True))
+        #----------zx----------#
+        x2 = x2 * self.attn_zx(x2)
+        #----------zy----------#
+        x3 = x3 * self.attn_zy(x3)
+        #----------dw----------#
+        x4 = self.dw(x4)
+        #----------concat----------#
+        x = torch.cat([x1,x2,x3,x4],dim=1)
+        #----------ldw----------#
+        x = self.norm2(x)
+        x = self.ldw(x)
+        return x
+
+
+class EFANet(nn.Module):
+    
+    def __init__(self, num_classes=1, input_channels=3, c_list=[8,16,24,32,48,64], bridge=True, gt_ds=True):
+        super().__init__()
+
+        self.bridge = bridge
+        self.gt_ds = gt_ds
+        
+        self.encoder1 = WTConv2d(input_channels, c_list[0], kernel_size=3)
+        self.encoder2 = WTConv2d(c_list[0], c_list[1], kernel_size=3)
+        self.encoder3 = WTConv2d(c_list[1], c_list[2], kernel_size=3)
+        self.encoder4 = MDFA(c_list[2], c_list[3])
+        self.encoder5 = MDFA(c_list[3], c_list[4])
+        self.encoder6 = MDFA(c_list[4], c_list[5])
+
+        if bridge: 
+            self.GAB1 = MLFA(c_list[1], c_list[0])
+            self.GAB2 = MLFA(c_list[2], c_list[1])
+            self.GAB3 = MLFA(c_list[3], c_list[2])
+            self.GAB4 = MLFA(c_list[4], c_list[3])
+            self.GAB5 = MLFA(c_list[5], c_list[4])
+            print('Multi-Level Feature Aggregation was used')
+        if gt_ds:
+            self.gt_conv1 = nn.Sequential(nn.Conv2d(c_list[4], 1, 1))
+            self.gt_conv2 = nn.Sequential(nn.Conv2d(c_list[3], 1, 1))
+            self.gt_conv3 = nn.Sequential(nn.Conv2d(c_list[2], 1, 1))
+            self.gt_conv4 = nn.Sequential(nn.Conv2d(c_list[1], 1, 1))
+            self.gt_conv5 = nn.Sequential(nn.Conv2d(c_list[0], 1, 1))
+            print('Deep Supervision was used')
+        
+        self.decoder1 = MDFA(c_list[5], c_list[4])
+        self.decoder2 = MDFA(c_list[4], c_list[3])
+        self.decoder3 = MDFA(c_list[3], c_list[2])
+        self.decoder4 = WTConv2d(c_list[2], c_list[1], kernel_size=3)
+        self.decoder5 = WTConv2d(c_list[1], c_list[0], kernel_size=3)
+        
+        self.ebn1 = nn.GroupNorm(4, c_list[0])
+        self.ebn2 = nn.GroupNorm(4, c_list[1])
+        self.ebn3 = nn.GroupNorm(4, c_list[2])
+        self.ebn4 = nn.GroupNorm(4, c_list[3])
+        self.ebn5 = nn.GroupNorm(4, c_list[4])
+        self.dbn1 = nn.GroupNorm(4, c_list[4])
+        self.dbn2 = nn.GroupNorm(4, c_list[3])
+        self.dbn3 = nn.GroupNorm(4, c_list[2])
+        self.dbn4 = nn.GroupNorm(4, c_list[1])
+        self.dbn5 = nn.GroupNorm(4, c_list[0])
+
+        self.final = nn.Conv2d(c_list[0], num_classes, kernel_size=1)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Conv1d):
+                n = m.kernel_size[0] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        
+        out = F.gelu(F.max_pool2d(self.ebn1(self.encoder1(x)),2,2))
+        t1 = out # b, c0, H/2, W/2
+
+        out = F.gelu(F.max_pool2d(self.ebn2(self.encoder2(out)),2,2))
+        t2 = out # b, c1, H/4, W/4 
+
+        out = F.gelu(F.max_pool2d(self.ebn3(self.encoder3(out)),2,2))
+        t3 = out # b, c2, H/8, W/8
+        
+        out = F.gelu(F.max_pool2d(self.ebn4(self.encoder4(out)),2,2))
+        t4 = out # b, c3, H/16, W/16
+        
+        out = F.gelu(F.max_pool2d(self.ebn5(self.encoder5(out)),2,2))
+        t5 = out # b, c4, H/32, W/32
+        
+        out = F.gelu(self.encoder6(out)) # b, c5, H/32, W/32
+        t6 = out
+        
+        out5 = F.gelu(self.dbn1(self.decoder1(out))) # b, c4, H/32, W/32
+        if self.gt_ds: 
+            gt_pre5 = self.gt_conv1(out5)
+            t5 = self.GAB5(t6, t5, gt_pre5)
+            gt_pre5 = F.interpolate(gt_pre5, scale_factor=32, mode ='bilinear', align_corners=True)
+        else: t5 = self.GAB5(t6, t5)
+        out5 = torch.add(out5, t5) # b, c4, H/32, W/32
+        
+        out4 = F.gelu(F.interpolate(self.dbn2(self.decoder2(out5)),scale_factor=(2,2),mode ='bilinear',align_corners=True)) # b, c3, H/16, W/16
+        if self.gt_ds: 
+            gt_pre4 = self.gt_conv2(out4)
+            t4 = self.GAB4(t5, t4, gt_pre4)
+            gt_pre4 = F.interpolate(gt_pre4, scale_factor=16, mode ='bilinear', align_corners=True)
+        else:t4 = self.GAB4(t5, t4)
+        out4 = torch.add(out4, t4) # b, c3, H/16, W/16
+        
+        out3 = F.gelu(F.interpolate(self.dbn3(self.decoder3(out4)),scale_factor=(2,2),mode ='bilinear',align_corners=True)) # b, c2, H/8, W/8
+        if self.gt_ds: 
+            gt_pre3 = self.gt_conv3(out3)
+            t3 = self.GAB3(t4, t3, gt_pre3)
+            gt_pre3 = F.interpolate(gt_pre3, scale_factor=8, mode ='bilinear', align_corners=True)
+        else: t3 = self.GAB3(t4, t3)
+        out3 = torch.add(out3, t3) # b, c2, H/8, W/8
+        
+        out2 = F.gelu(F.interpolate(self.dbn4(self.decoder4(out3)),scale_factor=(2,2),mode ='bilinear',align_corners=True)) # b, c1, H/4, W/4
+        if self.gt_ds: 
+            gt_pre2 = self.gt_conv4(out2)
+            t2 = self.GAB2(t3, t2, gt_pre2)
+            gt_pre2 = F.interpolate(gt_pre2, scale_factor=4, mode ='bilinear', align_corners=True)
+        else: t2 = self.GAB2(t3, t2)
+        out2 = torch.add(out2, t2) # b, c1, H/4, W/4 
+        
+        out1 = F.gelu(F.interpolate(self.dbn5(self.decoder5(out2)),scale_factor=(2,2),mode ='bilinear',align_corners=True)) # b, c0, H/2, W/2
+        if self.gt_ds: 
+            gt_pre1 = self.gt_conv5(out1)
+            t1 = self.GAB1(t2, t1, gt_pre1)
+            gt_pre1 = F.interpolate(gt_pre1, scale_factor=2, mode ='bilinear', align_corners=True)
+        else: t1 = self.GAB1(t2, t1)
+        out1 = torch.add(out1, t1) # b, c0, H/2, W/2
+        
+        out0 = F.interpolate(self.final(out1),scale_factor=(2,2),mode ='bilinear',align_corners=True) # b, num_class, H, W
+        
+        if self.gt_ds:
+            return (torch.sigmoid(gt_pre5), torch.sigmoid(gt_pre4), torch.sigmoid(gt_pre3),
+                    torch.sigmoid(gt_pre2), torch.sigmoid(gt_pre1)), torch.sigmoid(out0)
+        else:
+            return torch.sigmoid(out0)
+
+
+if __name__ == "__main__":
+    x = torch.randn(8, 3, 256, 256).cuda()
+    model = EFANet().cuda()
+    print(model(x)[1].shape)
